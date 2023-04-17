b60ec088e1b261dd8ed474bca0890d7ade33d93f
Edu Gonzalez Garcia <71343560+edgonga@users.noreply.github.com>
2023-04-17 13:10:04 +0200

    schema review

--- MongoDB/N1 - Estructuras/Youtube.js
+++ MongoDB/N1 - Estructuras/Youtube.js
@@ -242,3 +242,72 @@ db.createCollection("Youtube", {
 
     }
 })
+
+db.Youtube.insertOne({
+    UsuarioID: "abc123",
+    Usuario: {
+      Email: "example@gmail.com",
+      Password: "password123",
+      Nombre: "John Doe",
+      FechaNacimiento: new Date("1990-01-01"),
+      Sexo: "Masculino",
+      Pais: "Estados Unidos",
+      CP: "12345"
+    },
+    Videos: {
+      Titulo: "My First Video",
+      Descripcion: "This is my very first video on YouTube!",
+      Tamano: 1024,
+      NombreArchivo: "my_first_video.mp4",
+      Duracion: 60,
+      Thumbnail: "thumbnail.jpg",
+      Reproducciones: 0,
+      Likes: 0,
+      Dislikes: 0,
+      Estado: "publico",
+      Hashtag: {
+        Hashtag_id: "123",
+        Nombre: "#firstvideo"
+      },
+      Publicador: "John Doe",
+      HoraPublicacion: new Date("2022-01-01")
+    },
+    Canal: {
+      NombreCanal: "JohnDoeOfficial",
+      Descripcion: "Welcome to my official YouTube channel!",
+      FechaCreacion: new Date("2022-01-01")
+    },
+    Suscripciones: {
+      Canal: "JaneDoeOfficial",
+      UsuarioCanal: "JaneDoe"
+    },
+    Reacciones: {
+      Likes: {
+        Video: "my_first_video.mp4",
+        UsuarioVideo: "JaneDoe",
+        HoraReaccion: new Date("2022-01-01")
+      },
+      Dislike: []
+    },
+    Playlist: {
+      Video: "my_first_video.mp4",
+      FechaCreacion: new Date("2022-01-01"),
+      Estado: "publico"
+    },
+    Comentarios: {
+      Video: "my_first_video.mp4",
+      Texto: "Great video! Keep up the good work!",
+      HoraComentario: new Date("2022-01-01")
+    },
+    ReaccionesComentario: {
+      Likes: {
+        Video: "my_first_video.mp4",
+        UsuarioVideo: "JaneDoe",
+        UsuarioComentario: "JohnDoe",
+        Texto: "Thanks for the comment!",
+        HoraReaccion: new Date("2022-01-01")
+      },
+      Dislike: []
+    }
+  });
+  
